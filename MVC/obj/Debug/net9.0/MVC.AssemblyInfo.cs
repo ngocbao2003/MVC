95c013ea3c377a5371c633d3bbe5ad2a96ae9a0c
ngocbao2003 <nbao152003@gmail.com>
2025-02-16 12:23:26 +0700

    HTML home

--- MVC/obj/Debug/net9.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net9.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f16f2ebe2864c2cbec1717e07a75e90a818c12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af78f5dcf9493d7aca4c6a18c5cfe87e79832b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
